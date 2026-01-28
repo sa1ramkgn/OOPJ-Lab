226fde62087abcd7302d075f3884e498e4f71757
sa1ramkgn <sa1ramkgn07@gmail.com>
2026-01-28 12:33:01 +0530

    Add files via upload

---
+++ totalaverage.java
@@ -0,0 +1,21 @@
+class totalaverage
+{
+	static int a,b;
+	static int total;
+	static double average;
+	calculate()
+	{
+		total=a+b;
+		average=total/2.0;
+	}
+	public static void main(string[] args)
+	{
+		a=Integer.parse Int(args[0]);
+		b=Integer.parse Int(args[1]);
+		calculate();
+	}
+    System.out.println("first number:"+a)
+    System.out.println("first number:"+b)
+    System.out.println("total:")
+	System.out.println("average:")
+   }
