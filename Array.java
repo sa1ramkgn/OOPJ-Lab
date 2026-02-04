1f40d26c032c97206670aaea38e05e111d19fd74
sa1ramkgn <sa1ramkgn07@gmail.com>
2026-02-04 11:23:07 +0530

    week-2

---
+++ Array.java
@@ -0,0 +1,16 @@
+class Array
+{
+public static void main(String[] args) 
+{
+int[] num=new int[5];
+num[0]=1;
+num[1]=2;
+num[2]=3;
+num[3]=4;
+num[4]=5;
+for(int i=0;i<5;i++)
+{
+System.out.println(num[i]);
+}
+}
+}
